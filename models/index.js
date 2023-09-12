33dc1628e8e2dd1ac3a298ac68fe9804b49889a1
JacobDFlores <jacobfdaniel@gmail.com>
2023-09-11 20:49:19 -0500

    added associations to the models/index file

--- models/index.js
+++ models/index.js
@@ -5,29 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
   through: {
-    model: Trip,
+    model: ProductTag,
     unique: false
   },
-  // Define an alias for when data is retrieved
-  as: 'planned_trips'
 });
-
 // Tags belongToMany Products (through ProductTag) 
 Traveller.belongsToMany(Location, {
   // Define the third table needed to store the foreign keys
   through: {
-    model: Trip,
+    model: ProductTag,
     unique: false
   },
-  // Define an alias for when data is retrieved
-  as: 'planned_trips'
 });
 
 module.exports = {
